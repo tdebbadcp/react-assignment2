64906a2fef2c6c70f2ed762fde066cca75c2301f
Trilok Debbad <tdebbad@Triloks-MacBook-Pro.local>
2021-01-14 22:52:54 -0800

    Added fetchAll function that makes GET request

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
    const [characters, setCharacters] = useState([]);
@@ -15,6 +16,25 @@ function MyApp() {
    function updateList(person) {
       setCharacters([...characters, person]);
    }
+
+   async function fetchAll(){
+      try {
+         const response = await axios.get('http://localhost:5000/users');
+         return response.data.users_list;     
+      }
+      catch (error){
+         //We're not handling errors. Just logging into the console.
+         console.log(error); 
+         return false;         
+      }
+   }
+
+   useEffect(() => {
+      fetchAll().then( result => {
+         if (result)
+            setCharacters(result);
+       });
+   }, [] );
 
    return (
       <div className="container">
